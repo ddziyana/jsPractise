74d2510f98102cec277bac83d60efc18506bc242
dziyana <jajjajaja@gmail.com>
2025-08-08 19:02:28 +0300

    jj

--- js/zadachi.js
+++ js/zadachi.js
@@ -35,7 +35,7 @@ for (let i = 2; i <= 10; i++) {
     }
 } */
 
-    
+
 let i = 2;
 do {
     if (i % 2 !== 0) {
@@ -59,3 +59,73 @@ function fifthTask() {
 }
 arr.push(i)
 fifthTask()  //этот код просто скопировала и пыталась понять
+
+
+
+
+
+
+
+
+
+
+
+for (let i = 10; i >= 1; i--) {
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+//Задача: Напиши программу, которая выводит таблицу умножения от 1 до 5. Результат должен выглядеть примерно так:
+/* 
+1 x 1 = 1
+1 x 2 = 2
+1 x 3 = 3
+...
+2 x 1 = 2
+2 x 2 = 4
+...
+5 x 5 = 25 */
+
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= 5; j++) {
+        console.log(i + ':' j + '=' + i/j);
+    }
+};
+
+//Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+/* Your task is to write some code to help them:
+
+Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+Log the value of BMIMark and BMIJohn to the console.
+
+BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+ */
+
+const massMark = 78;
+const heightMark =1.69;
+const massJohn  =92;
+const heightJohn =1.95;
+const BMIMark = massMark/(heightMark*heightMark);
+const BMIJohn = massJohn/(heightJohn*heightJohn);
+console.log(BMIMark,BMIJohn);
+
+
+if (BMIMark>BMIJohn){
+    console.log("Mark's BMI is higher than John's!")
+} else {console.log("John's BMI is higher than Mark's!")}
+
+
+if (BMIMark>BMIJohn){
+    console.log("Mark's BMI" + BMIMark + "is higher than John's" + BMIJohn +"!")
+} else {console.log("John's BMI" + BMIJohn + "is higher than Mark's" + BMIMark +"!")}
