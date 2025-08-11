3db6cfdca02ea1453bfa445c1bea359cc5c228f4
dziyana <jajjajaja@gmail.com>
2025-08-11 19:08:25 +0300

    functions excrs

--- js/zadachi.js
+++ js/zadachi.js
@@ -129,3 +129,14 @@ if (BMIMark>BMIJohn){
 if (BMIMark>BMIJohn){
     console.log("Mark's BMI" + BMIMark + "is higher than John's" + BMIJohn +"!")
 } else {console.log("John's BMI" + BMIJohn + "is higher than Mark's" + BMIMark +"!")}
+
+
+//Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+const bill = 275;
+
+//condition ? exprIfTrue : exprIfFalse
+
+let tip = 50 <= bill && bill <= 300 ? 0.15*bill : 0.2*bill;
+
+console.log(bill + tip);
